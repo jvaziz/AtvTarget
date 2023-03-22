86c9272ce8105c5edb705d500006be5313fc0926
João Victor Aziz <88343526+jvaziz@users.noreply.github.com>
2023-03-22 16:13:52 -0300

    Add files via upload

---
+++ processo_seletivo/src/processo_seletivo/atividade1.java
@@ -0,0 +1,19 @@
+package processo_seletivo;
+
+public class atividade1 {
+
+	public static void main(String[] args) {
+		int i = 13;
+		int soma = 0;
+		int k = 0;
+		
+		while (k < i) {
+			k = k + 1;
+			soma = soma + k;
+		}
+		
+		System.out.println(soma);
+
+	}
+
+}
